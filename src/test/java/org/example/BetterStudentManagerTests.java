87d0d3bfccb07cb2d446ba821c3448fd24a9433c
swarnaburra <swarnaburra2025@gmail.com>
2026-01-03 01:33:21 -0600

    *Checked code with Negative test case testRemoveIncorrectStudentName *Checked code with Edge case testAddEmptyStringName

--- src/test/java/org/example/BetterStudentManagerTests.java
+++ src/test/java/org/example/BetterStudentManagerTests.java
@@ -2,7 +2,7 @@ package org.example;
 
 import org.junit.Test;
 
-public class BetterStudentManagerTests {
+public class  BetterStudentManagerTests {
     @Test
     public void testAddStudent() {
         BetterStudentManager  manager = new BetterStudentManager();
@@ -13,6 +13,21 @@ public class BetterStudentManagerTests {
         assert manager.students.get(0).grade == 90.9;
         assert manager.students.get(1).name.equals("Jane");
         assert manager.students.get(1).grade == 88.3;
+    }
+     @Test
+    public void testAddEmptyStringName(){
+        BetterStudentManager manager = new BetterStudentManager();
+        manager.addStudent("", 98.7);
+        manager.addStudent("", 88.8);
+        manager.addStudent("Cara", 83.7);
+        assert manager.students.size() == 3;
+        assert manager.students.get(0).name.isEmpty();
+        assert manager.students.get(0).grade == 98.7;
+        assert manager.students.get(1).name.isEmpty();
+        assert manager.students.get(1).grade == 88.8;
+        assert manager.students.get(2).name.equals("Cara");
+        assert manager.students.get(2).grade == 83.7;
+
     }
 
     @Test
@@ -25,12 +40,22 @@ public class BetterStudentManagerTests {
         assert manager.students.getFirst().name.equals("Jane");
         assert manager.students.getFirst().grade == 88.3;
     }
+@Test
+public void testRemoveIncorrectStudentName(){
+  BetterStudentManager manager = new BetterStudentManager();
+  manager.addStudent("Ira", 98.9);
+  manager.addStudent("Lara", 99.8);
+  manager.addStudent("Lara", 98.8);
+  manager.addStudent("Lara", 96.8);
+  manager.removeStudent("Lara");
+
+}
 
     @Test
     public void testGetStudentList() {
         BetterStudentManager  manager = new BetterStudentManager();
         manager.addStudent("John", 90.9);
         manager.addStudent("Jane", 88.3);
-        assert manager.getStudentList().equals("John 90.9\nJane 88.3\n");
+        assert manager.getStudentList().equals("\r\nJohn 90.9\r\nJane 88.3");
     }
 }
